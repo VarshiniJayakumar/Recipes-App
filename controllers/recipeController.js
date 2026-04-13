4a410a43f0f8fff787c75e4fc4879fb3dac85578
preesubs <varshinijayakumar16&gmail.com>
2026-04-13 17:50:01 +0530

    Added full backend code

---
+++ controllers/recipeController.js
@@ -0,0 +1,55 @@
+const Recipe = require('../models/Recipe');
+
+// Create a new recipe
+exports.createRecipe = async (req, res) => {
+  try {
+    const recipe = new Recipe(req.body);
+    await recipe.save();
+    res.status(201).json(recipe);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+};
+
+// Get all recipes
+exports.getAllRecipes = async (req, res) => {
+  try {
+    const recipes = await Recipe.find();
+    res.json(recipes);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+// Get recipe by ID
+exports.getRecipeById = async (req, res) => {
+  try {
+    const recipe = await Recipe.findById(req.params.id);
+    if (!recipe) return res.status(404).json({ error: 'Recipe not found' });
+    res.json(recipe);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+// Update recipe by ID
+exports.updateRecipe = async (req, res) => {
+  try {
+    const recipe = await Recipe.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    if (!recipe) return res.status(404).json({ error: 'Recipe not found' });
+    res.json(recipe);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+};
+
+// Delete recipe by ID
+exports.deleteRecipe = async (req, res) => {
+  try {
+    const recipe = await Recipe.findByIdAndDelete(req.params.id);
+    if (!recipe) return res.status(404).json({ error: 'Recipe not found' });
+    res.json({ message: 'Recipe deleted successfully' });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
